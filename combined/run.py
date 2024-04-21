b9cf27232210c6aa217512a7eb9aa48debcae410
vladislav3112 <dyushakolesov@mail.ru>
2024-04-21 22:01:03 +0300

    link idx number corrected

--- combined/run.py
+++ combined/run.py
@@ -26,12 +26,12 @@ def computeInterMatrix(Z, sd0):
         x = sd0[2 * idx, 0]
         y = sd0[2 * idx + 1, 0]
         L[2 * idx] = np.array([-1 / Z, 0, x / Z, y])
-        L[2 * idx + 1] = np.array([0, -1 / Z, x / Z, -x])
+        L[2 * idx + 1] = np.array([0, -1 / Z, y / Z, -x])
     return L
 
 
 def updateCamPos(cam):
-    linkState = p.getLinkState(boxId, linkIndex=6)
+    linkState = p.getLinkState(boxId, linkIndex=7)
     # pos
     xyz = linkState[0]
     # orientation
@@ -39,6 +39,15 @@ def updateCamPos(cam):
     rotMat = p.getMatrixFromQuaternion(quat)
     rotMat = np.reshape(np.array(rotMat), (3, 3))
     camera.set_new_position(xyz, rotMat)
+
+
+def depth(camera):
+    Z = np.zeros(4)
+    for idx in range(4):
+        mw = MARKER_CORNERS_WORLD[idx]
+        camMark = np.reshape(camera.viewMatrix, (4, 4)).T @ mw
+        Z[idx] = -camMark[2]
+    return Z
 
 
 camera = Camera(imgSize=[IMG_SIDE, IMG_SIDE])
@@ -53,7 +62,7 @@ yd = 0.5
 L = 0.5
 Z0 = 0.3
 pos = q0
-maxTime = 50
+maxTime = 100
 logTime = np.arange(0.0, maxTime, dt)
 sz = logTime.size
 logPos = np.zeros(sz)
@@ -78,7 +87,7 @@ p.setGravity(0, 0, -10)
 boxId = p.loadURDF("combined/simple.urdf.xml", useFixedBase=True)
 
 # add aruco cube and aruco texture
-c = p.loadURDF("combined/aruco.urdf", (0.5, 0.5, 0.0), useFixedBase=True)
+c = p.loadURDF("combined/aruco.urdf", (0.5, 0.5, 0), useFixedBase=True)
 x = p.loadTexture("combined/aruco_cube.png")
 p.changeVisualShape(c, -1, textureUniqueId=x)
 
@@ -95,7 +104,7 @@ p.setJointMotorControlArray(
     targetPositions=[0.0, 0.0, 1.5708, 0.0],
     controlMode=p.POSITION_CONTROL,
 )
-for _ in range(100):
+for _ in range(50):
     p.stepSimulation()
 
 updateCamPos(camera)
@@ -109,7 +118,7 @@ sd = np.reshape(np.array(corners[0][0]), (8, 1)).astype(int)
 p.setJointMotorControlArray(
     bodyIndex=boxId,
     jointIndices=jointIndices,
-    targetPositions=[0.1, 0.1, 1.4708, 0.1],
+    targetPositions=[0.05, 0.05, 1.4708, 0.05],
     controlMode=p.POSITION_CONTROL,
 )
 for _ in range(100):
@@ -131,10 +140,11 @@ for t in logTime[1:]:
         s = corners[0][0, 0]
         s0 = np.reshape(np.array(corners[0][0]), (8, 1))
         s0 = np.array([(ss - IMG_HALF) / IMG_HALF for ss in s0])
+        Z0 = depth(camera)[0]
         L0 = computeInterMatrix(Z0, s0)
         L0T = np.linalg.inv(L0.T @ L0) @ L0.T
         e = s0 - sd0
-        coef = 1 / 2
+        coef = 1 / 20
         w = -coef * L0T @ e
 
     jStates = p.getJointStates(boxId, jointIndices=jointIndices)
@@ -142,7 +152,7 @@ for t in logTime[1:]:
     jVel = [state[1] for state in jStates]
     (linJac, angJac) = p.calculateJacobian(
         bodyUniqueId=boxId,
-        linkIndex=7,
+        linkIndex=eefLinkIdx,
         localPosition=[0, 0, 0],
         objPositions=jPos,
         objVelocities=[0, 0, 0, 0],
@@ -154,9 +164,11 @@ for t in logTime[1:]:
     J = np.block(
         [[np.array(linJac)[:3, :3], np.zeros((3, 1))], [np.array(angJac)[2, :]]]
     )
-    dq = (np.linalg.pinv(J) @ w).flatten()[[1, 0, 2, 3]]
-    dq[2] = -dq[2]
+    dq = (np.linalg.inv(J) @ w).flatten()[[1, 0, 2, 3]]  # [[1, 0, 2, 3]]  # was
     dq[0] = -dq[0]
+    dq[2] = -dq[2]
+    #    dq[3] = -dq[3]
+
     p.setJointMotorControlArray(
         bodyIndex=boxId,
         jointIndices=jointIndices,
